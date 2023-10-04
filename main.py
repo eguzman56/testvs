dfc00fad0301d778fc1c18e3ea8b5d177d7f9015
Guzman <eguzman56@cps.k12.il.us>
2023-10-04 09:07:26 -0500

    added index.html

---
+++ main.py
@@ -0,0 +1,7 @@
+#data abstarction when you need to simplify data 
+#from something more complex
+#An example of data abstarction is when  I use a list to simplify data instead of having
+#to write out the same code over and over again
+#data abstraction is when you simplify data
+#if you have a list of names, you can simplify it by using a list
+print("Hello")
